c13118a1b12f3a4c3b6c6a1d3e7718b19992f235
Jesse Velleu <jvelleu@umich.edu>
2019-04-17 21:50:32 -0400

    sass config

---
+++ config.rb
@@ -0,0 +1,24 @@
+require 'compass/import-once/activate'
+# Require any additional compass plugins here.
+
+http_path = "/Users/jessevelleu/jvelleu.github.io/html"
+css_dir = "css"
+sass_dir = "scss"
+images_dir = "images"
+generated_images_path = "/Users/jessevelleu/jvelleu.github.io/generatedimages"
+javascripts_dir = "javascripts"
+fonts_dir = "fonts"
+
+output_style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass scss scss && rm -rf sass && mv scss sass
