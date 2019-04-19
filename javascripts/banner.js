0b0a3fb67fb0997fe51e742abdb53586a6ad384b
Jesse Velleu <jvelleu@umich.edu>
2019-04-19 15:04:00 -0400

    transition fade

--- javascripts/banner.js
+++ javascripts/banner.js
@@ -3,14 +3,51 @@
 var banner_index = 0;
 showSlides();
 
+function fadeOut(element) {
+	let opacity = 1;
+	function decrease () {
+		opacity -= 0.05;
+		if (opacity <= 0){
+			// complete
+			element.style.opacity = 0;
+			return true;
+		}
+		element.style.opacity = opacity;
+		requestAnimationFrame(decrease);
+	}
+	decrease();
+}
+
+function fadeIn(element) {
+	let opacity = 0;
+	function increase () {
+		opacity += 0.05;
+		if (opacity >= 1){
+			// complete
+			element.style.opacity = 1;
+			return true;
+		}
+		element.style.opacity = opacity;
+		requestAnimationFrame(increase);
+	}
+	increase();
+}
+
 function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("featured-item");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none"; 
-  }
-  banner_index++;
-  if (banner_index > slides.length) {banner_index = 1} 
-  slides[banner_index-1].style.display = "block"; 
-  setTimeout(showSlides, 10000); // Change image every 2 seconds
+	let i;
+	let banners = document.getElementsByClassName("featured-item");
+	let len = banners.length;
+	
+	for (i = 0; i < len; i++) {
+		//banners[i].style.display = "none";
+		fadeOut(banners[i]);
+	}
+	
+	banner_index++;
+	if (banner_index > len) {banner_index = 1} 
+	
+	//banners[banner_index-1].style.display = "block"; 
+	fadeIn(banners[i]);
+	
+	setTimeout(showSlides, 1000); // Change image every 10 seconds
 }
